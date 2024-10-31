e83a14715cbfd2be697b2e16848108d75193b99c
CodeDoes <codedoes@gmail.com>
2024-10-31 20:56:01 +0200

    Update index.ts fixes typo

--- src/index.ts
+++ src/index.ts
@@ -301,7 +301,7 @@ window.ipcRenderer.on('main-process-message', (_event, message) => {
         // Preload scripts may contain Web assets, so use the \`build.rollupOptions.input\` instead \`build.lib.entry\`.
         input: path.join(__dirname, 'electron/preload.ts'),
       },
-      // Ployfill the Electron and Node.js API for Renderer process.
+      // Polyfill the Electron and Node.js API for Renderer process.
       // If you want use Node.js in Renderer process, the \`nodeIntegration\` needs to be enabled in the Main process.
       // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
       renderer: process.env.NODE_ENV === 'test'
